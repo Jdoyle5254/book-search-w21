3207116e758950a205c15d1fa5a9ffcf1edb28c4
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-03-05 19:36:40 -0600

    set up server file and test get route

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    return ("hello are you working?")
+    console.log("you are here working now")
+})
+
+app.get("/api", (req, res) => {
+    return ("hello world from your server")
+})
+
 app.listen(PORT, () => {
     console.log(`Server Listening on ${PORT}`); 
+
 })
