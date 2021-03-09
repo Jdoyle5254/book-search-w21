f861263288cd68d033af96b93e22a7f632a007f9
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-03-08 18:14:45 -0600

    add to gitignore & created schema notes on api req

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,39 @@ import Header from "./components/Header"
 import SearchBookBox from "./components/SearchBookBox"
 
 function App() {
+  // const [search, setSearch] = useState({ search: "", limit: "" });
+  // const [gifs, setgifs] = useState([]);
+
+  // const handleSearch = event => {
+  //   setSearch({ ...search, search: event.target.value });
+  // };
+  // const handleLimit = event => {
+  //   setSearch({ ...search, limit: event.target.value });
+  // };
+
+
+  // const handleClick = async event => {
+  //   event.preventDefault();
+  //   try {
+  //     const response = await fetch(
+  //       `https://api.giphy.com/v1/gifs/search?api_key=ndBh69mQJzrSa8QGZLB2gZhhhmCBZQZb&q=${
+  //         search.search
+  //       }&limit=${search.limit}&offset=0&rating=g&lang=en`
+  //     );
+  //     const gifList = await response.json();
+  //     console.log("RESPONSE", gifList);
+  //     setgifs([...gifList.data]);
+  //     console.log("DATA", gifList.data);
+  //   } catch (err) {
+  //     console.log("error: ", err);
+  //   }
+  // };
+
+  // const handleClear = event => {
+  //   event.preventDefault();
+  //   setgifs([]);
+  // };
+
   return (
     <div className="App">
       <Header />
