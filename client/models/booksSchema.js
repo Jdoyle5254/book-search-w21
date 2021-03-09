100ab54649de26b998de40e71ddd3d2fe6e30f6e
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-03-08 18:21:16 -0600

    set up schema and local Mongo connection

--- client/models/booksSchema.js
+++ client/models/booksSchema.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// here we are creating the schema for the DB 
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+  thumbnail: { type:String},
+  link: { type: String}
 });
 
 const Book = mongoose.model("Book", bookSchema);
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001
 
+//TODO need to create route to local DB   need to create flow chart for api calls and server to db calls
+
 const app = express()
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +17,26 @@ app.get("/", (req,res) => {
 app.get("/api", (req, res) => {
     res.json("hello world from your server")
 })
+
+// Connect to the Mongo DB
+if (process.env.NODE_ENV === "production") {
+    mongoose.connect(process.env.MONGODB_URI,  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+  } else {
+    mongoose.connect("mongodb://localhost/booksearchdb",
+    {
+      //useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }) .then(()=>{
+      console.log("DB Connected Successfully");
+  });
+  }
 
 app.listen(PORT, () => {
     console.log(`Server Listening on ${PORT}`); 
